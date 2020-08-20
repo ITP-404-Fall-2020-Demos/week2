7b82f3f3d37b06ec305b2d399b18c8d705c01234
David Tang <dtang@auditboard.com>
2020-08-20 00:18:18 -0700

    Change template tag to script tag

--- main.js
+++ main.js
@@ -3,12 +3,6 @@ const membersTemplate = Handlebars.compile($("#members-template").html(), {
   strict: true,
 });
 
-// const html = memberTemplate({
-//   login: "abuiles",
-//   // login: "<script>alert('hi')</script>",
-//   avatar_url: "https://avatars0.githubusercontent.com/u/21772?v=4",
-// });
-
 const html = membersTemplate({
   members: [
     {
