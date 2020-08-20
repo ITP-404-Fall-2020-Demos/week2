3dbe1fa5536cfac9446fe5de2497066e941f1098
David Tang <dtang@auditboard.com>
2020-08-19 23:32:18 -0700

    2. The each helper

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const memberTemplate = Handlebars.compile($("#member-template").html());
+const membersTemplate = Handlebars.compile($("#members-template").html(), {
+  strict: true,
+});
 
-const html = memberTemplate({
-  login: "abuiles",
-  // login: "<script>alert('hi')</script>",
-  avatar_url: "https://avatars0.githubusercontent.com/u/21772?v=4",
+// const html = memberTemplate({
+//   login: "abuiles",
+//   // login: "<script>alert('hi')</script>",
+//   avatar_url: "https://avatars0.githubusercontent.com/u/21772?v=4",
+// });
+
+const html = membersTemplate({
+  members: [
+    {
+      login: "abuiles",
+      avatar_url: "https://avatars0.githubusercontent.com/u/21772?v=4",
+    },
+    {
+      login: "acorncom",
+      avatar_url: "https://avatars1.githubusercontent.com/u/802505?v=4",
+    },
+  ],
 });
 
 $("#results").html(html);
